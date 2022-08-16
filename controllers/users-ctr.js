261dfe0d1a2d16fdf65ada803b96fe704c638b5b
azatyan viola <viola.azatyan@mail.ru>
2022-08-16 06:14:54 -0700

    Change sendgrid function

--- controllers/users-ctr.js
+++ controllers/users-ctr.js
@@ -47,13 +47,19 @@ class UsersCtrl {
             });
          
         const token = createToken({ id: user._id, email: user.email });
+    
 
         // Send email (use credintials of SendGrid)
-        const transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: process.env.SENDGRID_USERNAME, pass: process.env.SENDGRID_PASSWORD } });
-        const mailOptions = { from: 'ani@startupbenefits.eu', to: user.email, subject: 'Account Verification Link', text: 'Hello '+ req.body.name +',\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + user.email + '\/' + token.token + '\n\nThank You!\n' };
+        const transporter = nodemailer.createTransport(
+            sendgridTransport({
+                auth:{
+                    api_key:process.env.SENDGRID_APIKEY,
+                }
+            })
+        )
+        let mailOptions = { from: 'ani@startupbenefits.eu', to: user.email, subject: 'Account Verification Link', text: 'Hello '+ req.body.name +',\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + user.email + '\/' + token.token + '\n\nThank You!\n' };
         }
-
-    }
+    }    
     static async  usersLogin(req, res) {
         const {email} = req.body;
         try {
@@ -70,8 +76,9 @@ class UsersCtrl {
                     .send({
                         data: token,
                     });
-            } else if (!user.isVerified){
-                return res.status(401).send({msg:'Your Email has not been verified. Please click on resend'});
+             }
+            if (!user.isVerified){
+                return res.status(401).send({message:'Your Email has not been verified. Please click on resend'});
             } else {
                 return res
                     .status(404)
@@ -100,18 +107,18 @@ class UsersCtrl {
         const {token} = req.body.token;
             // token is not found into database i.e. token may have expired 
             if (!token){
-                return res.status(400).send({msg:'Your verification link may have expired. Please click on resend for verify your Email.'});
+                return res.status(400).send({message:'Your verification link may have expired. Please click on resend for verify your Email.'});
             }
             // if token is found then check valid user 
             else{
-                Users.findOne({ _id: token._userId, email: req.params.email }, function (err, user) {
+                Users.findOne({ _id: token._userId, email: req.body }, function (err, user) {
                     // not valid user
                     if (!user){
-                        return res.status(401).send({msg:'We were unable to find a user for this verification. Please SignUp!'});
+                        return res.status(401).send({message:'We were unable to find a user for this verification. Please SignUp!'});
                     } 
                     // user is already verified
                     else if (user.isVerified){
-                        return res.status(200).send('User has been already verified. Please Login');
+                        return res.status(200).send({message:'User has been already verified. Please Login'});
                     }
                     // verify user
                     else{
@@ -120,11 +127,11 @@ class UsersCtrl {
                         user.save(function (err) {
                             // error occur
                             if(err){
-                                return res.status(500).send({msg: err.message});
+                                return res.status(500).send({message: err.message});
                             }
                             // account successfully verified
                             else{
-                              return res.status(200).send('Your account has been successfully verified');
+                              return res.status(200).send({message:'Your account has been successfully verified'});
                             }
                         });
                     }
@@ -138,11 +145,11 @@ class UsersCtrl {
         Users.findOne({ email: req.body.email }, function (err, user) {
             // user is not found into database
             if (!user){
-                return res.status(400).send({msg:'We were unable to find a user with that email. Make sure your Email is correct!'});
+                return res.status(400).send({message:'We were unable to find a user with that email. Make sure your Email is correct!'});
             }
             // user has been already verified
             else if (user.isVerified){
-                return res.status(200).send('This account has been already verified. Please log in.');
+                return res.status(200).send({message:'This account has been already verified. Please log in.'});
         
             } 
             // send verification link
@@ -155,9 +162,9 @@ class UsersCtrl {
                         const mailOptions = { from: 'no-reply@example.com', to: user.email, subject: 'Account Verification Link', text: 'Hello '+ user.name +',\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + user.email + '\/' + token.token + '\n\nThank You!\n' };
                         transporter.sendMail(mailOptions, function (err) {
                            if (err) { 
-                            return res.status(500).send({msg:'Technical Issue!, Please click on resend for verify your Email.'});
+                            return res.status(500).send({message:'Technical Issue!, Please click on resend for verify your Email.'});
                          }
-                        return res.status(200).send('A verification email has been sent to ' + user.email + '. It will be expire after one day. If you not get verification Email click on resend token.');
+                        return res.status(200).send({message:'A verification email has been sent to ' + user.email + '. It will be expire after one day. If you not get verification Email click on resend token.'});
                     });
                 }
             });
